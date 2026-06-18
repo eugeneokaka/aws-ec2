b80e4ab62d9efeb8f336aefa4ec36232f1bbe2cd
eugene okaka <eugeneokaka@gmail.com>
2026-06-18 19:27:50 +0300

    did not add about now added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const app = express();
-
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    res.json({ message: "Hello World!" });
-});
-
-app.listen(port, () => {
+const express=require('express');
+const app=express();
+port=process.env.PORT || 3000;
+app.get('/',(req,res)=>{
+    res.json({message:"Hello World!"})
+})
+app.get('/about',(req,res)=>{
+    res.json({message:"This is the about page"})
+})
+app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
-});
+})
